11a803798d4a3644748f793756eb343cef6f694c
Victor Engmark <vengmark@linz.govt.nz>
2021-12-14 14:45:01 +1300

    fix: Verify collection processing properties everywhere
    This makes sure that *any* invalid processing property causes a validation
    error.

--- tests/collection.test.js
+++ tests/collection.test.js
@@ -22,4 +22,158 @@ describe('Collection example', () => {
 
 		expect(valid).toBeTruthy();
 	});
+
+	it('should fail validation when providers processing expression is invalid', async () => {
+		// given
+		example.providers[0]['processing:expression'] = null;
+
+		// when
+		let valid = validate(example);
+
+		// then
+		expect(valid).toBeFalsy();
+		expect(
+			validate.errors.some(
+				(error) =>
+					error.instancePath === '/providers/0/processing:expression'
+					&& error.message === 'must be object',
+			)
+		).toBeTruthy();
+	});
+
+	it('should fail validation when asset processing expression is invalid', async () => {
+		// given
+		example.assets = {
+			'example': {
+				'href': 'https://example.org/file.xyz',
+				'processing:expression': null,
+			}
+		};
+
+		// when
+		let valid = validate(example);
+
+		// then
+		expect(valid).toBeFalsy();
+		expect(
+			validate.errors.some(
+				(error) =>
+					error.instancePath === '/assets/example/processing:expression'
+					&& error.message === 'must be object',
+			)
+		).toBeTruthy();
+	});
+
+	it('should fail validation when item asset processing expression is invalid', async () => {
+		// given
+		example.item_assets = {
+			'example': {
+				'href': 'https://example.org/file.xyz',
+				'processing:expression': null,
+			}
+		};
+
+		// when
+		let valid = validate(example);
+
+		// then
+		expect(valid).toBeFalsy();
+		expect(
+			validate.errors.some(
+				(error) =>
+					error.instancePath === '/item_assets/example/processing:expression'
+					&& error.message === 'must be object',
+			)
+		).toBeTruthy();
+	});
+
+	it('should fail validation when summary processing expression is invalid', async () => {
+		// given
+		example.summaries['processing:expression'] = null;
+
+		// when
+		let valid = validate(example);
+
+		// then
+		expect(valid).toBeFalsy();
+		expect(
+			validate.errors.some(
+				(error) =>
+					error.instancePath === '/summaries/processing:expression'
+					&& error.message === 'must be array',
+			)
+		).toBeTruthy();
+	});
+
+	it('should fail validation when summary processing facility is invalid', async () => {
+		// given
+		example.summaries['processing:facility'] = null;
+
+		// when
+		let valid = validate(example);
+
+		// then
+		expect(valid).toBeFalsy();
+		expect(
+			validate.errors.some(
+				(error) =>
+					error.instancePath === '/summaries/processing:facility'
+					&& error.message === 'must be array',
+			)
+		).toBeTruthy();
+	});
+
+	it('should fail validation when summary processing level is invalid', async () => {
+		// given
+		example.summaries['processing:level'] = null;
+
+		// when
+		let valid = validate(example);
+
+		// then
+		expect(valid).toBeFalsy();
+		expect(
+			validate.errors.some(
+				(error) =>
+					error.instancePath === '/summaries/processing:level'
+					&& error.message === 'must be array',
+			)
+		).toBeTruthy();
+	});
+
+	it('should fail validation when summary processing lineage is invalid', async () => {
+		// given
+		example.summaries['processing:lineage'] = null;
+
+		// when
+		let valid = validate(example);
+
+		// then
+		expect(valid).toBeFalsy();
+		expect(
+			validate.errors.some(
+				(error) =>
+					error.instancePath === '/summaries/processing:lineage'
+					&& error.message === 'must be array',
+			)
+		).toBeTruthy();
+	});
+
+	it('should fail validation when summary processing software is invalid', async () => {
+		// given
+		example.summaries['processing:software'] = null;
+
+		// when
+		let valid = validate(example);
+
+		// then
+		expect(valid).toBeFalsy();
+		expect(
+			validate.errors.some(
+				(error) =>
+					error.instancePath === '/summaries/processing:software'
+					&& error.message === 'must be array',
+			)
+		).toBeTruthy();
+	});
 });
