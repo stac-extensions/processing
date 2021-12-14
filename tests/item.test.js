64e05039bcddeaca8fd9b146db7bcffa7beeedd4
Victor Engmark <vengmark@linz.govt.nz>
2021-12-14 14:21:11 +1300

    tests: Verify that item example is valid

---
+++ tests/item.test.js
@@ -0,0 +1,25 @@
+const {join} = require('path');
+const {promises} = require('fs');
+const {AjvOptions, rootDirectory, schemaPath} = require('./validation.js');
+const ajv = new (require('ajv'))(AjvOptions);
+
+const examplePath = join(rootDirectory, 'examples/item.json');
+
+let validate;
+beforeAll(async () => {
+	const data = JSON.parse(await promises.readFile(schemaPath));
+	validate = await ajv.compileAsync(data);
+});
+
+let example;
+beforeEach(async () => {
+	example = JSON.parse(await promises.readFile(examplePath));
+});
+
+describe('Item example', () => {
+	it('should pass validation', async () => {
+		let valid = validate(example);
+
+		expect(valid).toBeTruthy();
+	});
+});
